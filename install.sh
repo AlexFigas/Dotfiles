158490b85adf3baf73bb00d9f262666eb0fba8c8
Alexandre Figueiredo <65889776+AlexFigas@users.noreply.github.com>
2022-03-12 23:02:52 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,6 +27,12 @@ sudo apt-get update; \
   sudo apt-get install -y apt-transport-https && \
   sudo apt-get update && \
   sudo apt-get install -y dotnet-sdk-6.0
+# Mono (.NET)
+sudo apt update
+sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+sudo apt-add-repository 'deb https://download.mono-project.com/repo/ubuntu stable-focal main'
+sudo apt install mono-complete 
 
 # VS Code ################################################################################
 sudo apt update
