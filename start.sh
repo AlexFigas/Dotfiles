389d6f3936ed285ef5d48f494294f803859c1edb
alexfigas <alexfigas11@gmail.com>
2022-03-12 18:12:54 +0000

    Updated permissions

--- start.sh
+++ start.sh
@@ -5,6 +5,9 @@
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 echo "";
 if [[ $REPLY =~ ^[Yy]$ ]]; then
+	chmod +x ./install.sh;
+	chmod +x ./update.sh;
+
 	./install.sh;
 	./update.sh;
 fi
