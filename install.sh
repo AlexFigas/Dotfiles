f141e71f675e6dae510a1ef97ff7e750be92e0f4
Alexandre Figueiredo <alexfigas11@gmail.com>
2022-05-27 10:17:12 +0100

    Added Spotify deb download

--- install.sh
+++ install.sh
@@ -149,6 +149,11 @@ sudo usermod -a -G plugdev $USER
 # Joplin
 wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash
 
+# Spotify
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
 # Final update, upgrade and clean junk ################################################################################
 sudo apt update && sudo apt upgrade -y
 sudo apt autopurge
